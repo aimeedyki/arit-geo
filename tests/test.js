bb5f9269d175be15a8b3c9e148654911006b9aa5
Amarachi <akuwudike.a@gmail.com>
2017-07-31 18:08:22 +0100

    passes all the tests

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,6 @@
 const assert = require('chai').assert;
 const myApp = require('../src/Aritgeo.js');
 
-console.log(myApp.aritGeo([2,4,6,8]))
 describe("Arithmetic or Geometric", function(){
 
 	describe("handle invalid input", function(){
@@ -59,8 +58,8 @@ describe("Arithmetic or Geometric", function(){
 	})
 
 	describe("handle invalid input", function(){
-		it ("should return error for '1,2,3,4']}", function(){
-			assert.equal(undefined, myApp.aritGeo('1,2,3,4'));
+		it ("should return error for '1234']}", function(){
+			assert.equal('Array of numbers is required', myApp.aritGeo('1234'));
 		});
 	})
 })
